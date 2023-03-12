9e83c28a59913b3c966417ff15d50cfb1a59a920
codeash14 <ayushiash1412@gmail.com>
2023-03-12 19:15:15 +0530

    final commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,8 +3,8 @@ import os
 
 class Config:
     SECRET_KEY = "oijxijxiaxhsah"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:ayushi1412@localhost:5432/flaskblogapp"
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
+    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:ayushi1412@localhost:5432/flaskblogapp"
+    SQLALCHEMY_DATABASE_URI = "postgresql://codeash:Rux7mY8OqJSWHHVNEzw4cy4eiSqANsVn@dpg-cg5nnqhmbg53u2rjpna0-a.singapore-postgres.render.com/whatsupp_db"
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
